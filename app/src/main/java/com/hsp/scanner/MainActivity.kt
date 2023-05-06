5bf537a9324018f8589363bc04102af35498e33e
DESKTOP-NEC290S\HSP <1101121039@qq.com>
2023-05-06 10:06:18 +0800

    升级AGP版本

--- app/src/main/java/com/hsp/scanner/MainActivity.kt
+++ app/src/main/java/com/hsp/scanner/MainActivity.kt
@@ -3,25 +3,21 @@ package com.hsp.scanner
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.blankj.utilcode.util.FileUtils
-import com.blankj.utilcode.util.ImageUtils
 import com.bumptech.glide.Glide
 import com.hnsh.scanner.CaptureActivity
 import com.hnsh.scanner.DeviceScanHelper
 import com.hnsh.scanner.decode.DecodeHandler
 import com.hnsh.scanner.zbarUtils.Constants
 import com.hnsh.scanner.zbarUtils.utils.BeepManager
-import kotlinx.android.synthetic.main.activity_main.*
+import com.hsp.scanner.databinding.ActivityMainBinding
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
-import java.io.File
 
 /**
  * @Description:
@@ -32,6 +28,11 @@ import java.io.File
  */
 class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks,
     View.OnClickListener {
+    
+    private val viewBinding by lazy {
+        ActivityMainBinding.inflate(LayoutInflater.from(this@MainActivity))
+    }
+    
     companion object {
         /**
          * 注意添加必需权限
@@ -40,34 +41,35 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks,
             Manifest.permission.CAMERA,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
-
+        
         const val SCAN_REQUEST_CODE = 1001
     }
-
+    
     private lateinit var scanHelper: DeviceScanHelper
     private var beepManager: BeepManager? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btn_scanner.setOnClickListener(this)
-        btn_play.setOnClickListener(this)
+        setContentView(viewBinding.root)
         initScanner()
+        viewBinding.btnScanner.setOnClickListener(this)
+        viewBinding.btnPlay.setOnClickListener(this)
+        Log.d("TAG", "onCreate: ------------------->${viewBinding}")
     }
-
+    
     private fun initScanner() {
         scanHelper = DeviceScanHelper(this)
         scanHelper.setOnScanSuccessListener(object : DeviceScanHelper.OnScanSuccessListener {
             override fun onSuccess(result: String?) {
                 result?.let {
                     Toast.makeText(this@MainActivity, it, Toast.LENGTH_SHORT).show()
-                    tv_result.text = it
+                    viewBinding.tvResult.text = it
                 }
             }
         })
-
+        
     }
-
+    
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == SCAN_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
@@ -76,10 +78,10 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks,
                 Toast.makeText(this@MainActivity, "扫描结果为空~", Toast.LENGTH_SHORT).show()
             } else {
                 val barCode = bundle.getString(DecodeHandler.BAR_CODE_KEY) ?: ""
-                tv_result.text = barCode
-
+                viewBinding.tvResult.text = barCode
+                
                 val albumPicPath = bundle.getString(DecodeHandler.ALBUM_PIC_KEY) ?: ""
-
+                
                 Log.d(
                     "TAG",
                     "onActivityResult: ----------------------barCode---->${barCode}"
@@ -89,33 +91,37 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks,
                     "onActivityResult: ----------------------albumPicPath---->${albumPicPath}"
                 )
                 if (albumPicPath.isNotEmpty()) {
-                    Glide.with(this).load(albumPicPath).into(iv_result)
+                    Glide.with(this).load(albumPicPath).into(viewBinding.ivResult)
                 }
             }
         }
     }
-
+    
     override fun onResume() {
         super.onResume()
         scanHelper.onResume()
     }
-
+    
     override fun onPause() {
         super.onPause()
         scanHelper.onPause()
     }
-
+    
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
         if (requestCode == SCAN_REQUEST_CODE) {
             CaptureActivity.start(this@MainActivity, SCAN_REQUEST_CODE, true)
         }
     }
-
+    
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        Toast.makeText(this@MainActivity, "应用没有拍照权限，请在手机设置中修改~", Toast.LENGTH_SHORT).show()
-
+        Toast.makeText(
+            this@MainActivity,
+            "应用没有拍照权限，请在手机设置中修改~",
+            Toast.LENGTH_SHORT
+        ).show()
+        
     }
-
+    
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -124,22 +130,31 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks,
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
-
+    
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.btn_scanner -> {
+            viewBinding.btnScanner.id -> {
+                Log.d(
+                    "TAG",
+                    "onClick: ----------btnScanner--------->${viewBinding.btnScanner.text}"
+                )
+                
                 checkScannerPermission()
             }
-            R.id.btn_play -> {
+            
+            viewBinding.btnPlay.id -> {
+                Log.d("TAG", "onClick: ----------btnPlay--------->${viewBinding.btnPlay.text}")
+                
                 // 播放扫描成语音
                 beepManager = BeepManager(this, R.raw.scan_success)
                 beepManager?.autoPlay()
             }
+            
             else -> {
             }
         }
     }
-
+    
     @AfterPermissionGranted(SCAN_REQUEST_CODE)
     private fun checkScannerPermission() {
         if (EasyPermissions.hasPermissions(this, *CAMERA_PERMISSIONS)) {
@@ -153,7 +168,7 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks,
             )
         }
     }
-
+    
     override fun onDestroy() {
         super.onDestroy()
         beepManager?.close()
